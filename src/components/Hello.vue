a06de5aa5241d6fc6b0e232aa424bbcc49549bfc
chibingrui1 <xuegang1@139.com>
2017-08-27 20:39:49 +0800

    增加了一个仓库和一个api文件夹

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,59 +1,9 @@
 <template>
-  
-    <el-row class="tac">
-      <el-col :span="8">
-        <h5>不带 icon</h5>
-        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" theme="dark">
-          <el-submenu index="1">
-            <template slot="title">导航一</template>
-            <el-menu-item-group title="分组一">
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="2">导航二</el-menu-item>
-          <el-menu-item index="3">导航三</el-menu-item>
-        </el-menu>
-      </el-col>
-      <el-switch
-        v-model="value1"
-        on-text=""
-        off-text="">
-      </el-switch>
-      <el-switch
-        v-model="value2"
-        on-color="#13ce66"
-        off-color="#ff4949">
-      </el-switch>
-    </el-row>
-
 
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        value1: true,
-        value2: true
-      }
-    },
-    methods: {
-      handleOpen (key, keyPath) {
-        console.log(key, keyPath)
-      },
-      handleClose (key, keyPath) {
-        console.log(key, keyPath)
-      }
-    }
-  }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
