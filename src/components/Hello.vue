9517983eebbf0d50e364e6794b918f5ea47a1ec2
chibingrui1 <xuegang1@139.com>
2017-08-27 17:23:57 +0800

    增加了mintui和elementui库

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router我</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex要</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader喊</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue子</a></li>
-    </ul>
-  </div>
+  
+    <el-row class="tac">
+      <el-col :span="8">
+        <h5>不带 icon</h5>
+        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" theme="dark">
+          <el-submenu index="1">
+            <template slot="title">导航一</template>
+            <el-menu-item-group title="分组一">
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">导航二</el-menu-item>
+          <el-menu-item index="3">导航三</el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-switch
+        v-model="value1"
+        on-text=""
+        off-text="">
+      </el-switch>
+      <el-switch
+        v-model="value2"
+        on-color="#13ce66"
+        off-color="#ff4949">
+      </el-switch>
+    </el-row>
+
+
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    data () {
+      return {
+        value1: true,
+        value2: true
+      }
+    },
+    methods: {
+      handleOpen (key, keyPath) {
+        console.log(key, keyPath)
+      },
+      handleClose (key, keyPath) {
+        console.log(key, keyPath)
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
